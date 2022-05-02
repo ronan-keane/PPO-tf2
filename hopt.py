08c6b2ff50de5f8dd699316e752a67aa48da92a5
ronan-keane <rlk268@cornell.edu>
2022-05-02 01:37:17 -0400

    hyperparameter optimization

---
+++ hopt.py
@@ -0,0 +1,36 @@
+
+from nni.experiment import Experiment
+
+search_space = {
+    'lr': {'_type': 'loguniform', '_value': [1e-5, 1e-3]},
+    'stdev_offset': {'_type': 'uniform', '_value':[0.2, 0.8]},
+    'gamma': {'_type': 'loguniform', '_value':[0.98, 0.998]},
+    'kappa': {'_type': 'loguniform', '_value':[0.2, 0.98]},
+    'global_clipnorm': {'_type': 'choice', '_value':[None, 1, 10]},
+    'units': {'_type': 'choice', '_value':[400, 600, 800, 500, 200]},
+    'ppo_clip': {'_type': 'uniform', '_value':[.1, .5]},
+    'means_acitvation': {'_type': 'uniform', '_value':[1., 1.5]}
+    }
+
+experiment = Experiment('local')
+exp_id = experiment.id
+experiment.config.trial_command = 'python hopt_eval.py'
+experiment.config.trial_code_directory = '.'
+experiment.config.search_space = search_space
+experiment.config.tuner.name = 'TPE'
+experiment.config.tunder.class_args['optimize_mode'] = 'maximize'
+experiment.config.assessor.name = 'Medianstop'
+experiment.config.tuner.class_args= {'optimize_mode':'maximize', 'start_step':500}
+
+experiment.config.max_trial_number = 100
+experiment.config.trial_concurrency = 1
+experiment.config.max_experiment_duration = '24h'
+experiment.config.use_active_gpu=False
+
+
+# You can use the web portal to view experiment status: http://localhost:8080.
+experiment.run(8080)
+
+#experiment.view(exp_id)
+#experiment.stop()
+#experiment.resume(exp_id)
--- hopt_eval.py
+++ hopt_eval.py
@@ -1,13 +1,11 @@
-"""Trains an agent on specified environment."""
 import tensorflow as tf
 import numpy as np
 import gym
-from train_setup import train_setup, LinearDecreaseLR, plot_ep_rewards
-import tqdm
+from train_setup import train_setup, LinearDecreaseLR
+import nni
 
 
 if __name__ == '__main__':
-    # documentation in train_setup
     ############ ENVIRONMENT ###################
     n_envs = 4
     env_list = [gym.make('BipedalWalker-v3') for i in range(n_envs)]
@@ -15,27 +13,27 @@ if __name__ == '__main__':
     action_dim = 4
     T = 1600
     env_kwargs = {}
+    ############ NNI HYPERPARAMETERS ###########
+    params = nni.get_next_parameter()
+    lr_max_policy = lr_min_policy = lr_max_value = lr_min_value = params['lr']
+    stdev_offset = params['stdev_offset']
+    gamma = params['gamma']
+    kappa = params['kappa']
+    global_clipnorm = params['global_clipnorm']
+    ppo_clip = params['ppo_clip']
+    means_activation = params['means_activation']
+    units = params['units']
+    policy_num_hidden = value_num_hidden = [units, units/2, units/4]
     ############ HYPERPARAMETERS ###############
-    policy_num_hidden = [500, 250, 100]
     policy_activation = 'relu'
-    action_clip = 'tanh'
-    means_activation = None
+    action_clip = 'clip'
     stdev_type = 'constant'
-    stdev_offset = 0.69
-    stdev_min = 1e-2
+    stdev_min = 1e-6
     value_num_hidden = [500, 250, 100]
     value_activation = 'relu'
     value_normalization = False
     value_type = 'time-aware'
-    gamma = 0.992
-    kappa = 0.5
-    ppo_clip = 0.2
-    global_clipnorm = None
     optimizer = tf.keras.optimizers.Adam
-    lr_max_policy = 1e-4  # policy learning rate on first iteration
-    lr_min_policy = 1e-4  # policy learning rate on last iteration
-    lr_max_value = 1e-4
-    lr_min_value = 1e-4
     nepochs = 10  # number of epochs to train each iteration
     nsteps = 1000  # each iteration samples nsteps transitions from each environment
     batch_size = 32  # mini-batch size for gradient updates
@@ -52,18 +50,12 @@ if __name__ == '__main__':
                 gamma, kappa, ppo_clip, global_clipnorm, optimizer, policy_lr, value_lr)
     # training loop and reporting
     n_updates = total_transitions // (n_envs*nsteps)
-    ep_rewards_list = []
-    pbar = tqdm.tqdm(range(n_updates))
-    pbar.set_description('Calculating first iteration')
+    pbar = range(n_updates)
     for i in pbar:
         cur_states = ppo.step(cur_states, nepochs, nsteps, batch_size)
         ep_rewards, ep_lens, ev, new_rewards = ppo.env.return_statistics()
-        ep_rewards_list.append(ep_rewards)
-        pbar.set_description('Iteration {:.0f}'.format(i+1))
-        pbar.set_postfix_str('Avg ep reward={:.0f}, Avg ep len={:.0f}, Explained var={:.2f}'.format(
-            np.mean(ep_rewards), np.mean(ep_lens), np.mean(ev))+', New ep rewards: '+new_rewards)
+        nni.report_intermediate_result(np.mean(ep_rewards))
         if np.mean(ep_rewards) > reward_threshold:
             break
 
-    plot_ep_rewards(ep_rewards_list, n_envs, nsteps)
-
+    nni.report_final_result(np.mean(ep_rewards))
