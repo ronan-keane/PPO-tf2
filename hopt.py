e118c889284cbbd76f4bc0f46ed02ccb90c40091
ronan-keane <55560863+ronan-keane@users.noreply.github.com>
2022-05-02 09:47:02 -0400

    fix hopt

--- hopt.py
+++ hopt.py
@@ -6,10 +6,10 @@ search_space = {
     'stdev_offset': {'_type': 'uniform', '_value':[0.2, 0.8]},
     'gamma': {'_type': 'loguniform', '_value':[0.98, 0.998]},
     'kappa': {'_type': 'loguniform', '_value':[0.2, 0.98]},
-    'global_clipnorm': {'_type': 'choice', '_value':[None, 1, 10]},
+    'global_clipnorm': {'_type': 'choice', '_value':[0, 1, 10]},
     'units': {'_type': 'choice', '_value':[400, 600, 800, 500, 200]},
     'ppo_clip': {'_type': 'uniform', '_value':[.1, .5]},
-    'means_acitvation': {'_type': 'uniform', '_value':[1., 1.5]}
+    'means_activation': {'_type': 'uniform', '_value':[1., 1.5]}
     }
 
 experiment = Experiment('local')
@@ -20,16 +20,16 @@ experiment.config.search_space = search_space
 experiment.config.tuner.name = 'TPE'
 experiment.config.tuner.class_args['optimize_mode'] = 'maximize'
 experiment.config.assessor.name = 'Medianstop'
-experiment.config.tuner.class_args= {'optimize_mode':'maximize', 'start_step':500}
+experiment.config.assessor.class_args= {'optimize_mode':'maximize', 'start_step':500}
 
-experiment.config.max_trial_number = 100
+experiment.config.max_trial_number = 200
 experiment.config.trial_concurrency = 1
-experiment.config.max_experiment_duration = '24h'
+experiment.config.max_experiment_duration = '48h'
 
 
 
-# You can use the web portal to view experiment status: http://localhost:8080.
-experiment.run(8080)
+# You can use the web portal to view experiment status: http://localhost:8088.
+experiment.run(8088)
 
 #experiment.view(exp_id)
 #experiment.stop()
--- hopt_eval.py
+++ hopt_eval.py
@@ -4,67 +4,66 @@ import gym
 from train_setup import train_setup, LinearDecreaseLR
 import nni
 
-if __name__ == '__main__':
-    try:
-        # Disable all GPUS
-        tf.config.set_visible_devices([], 'GPU')
-        visible_devices = tf.config.get_visible_devices()
-        for device in visible_devices:
-            assert device.device_type != 'GPU'
-    except:
-        # Invalid device or cannot modify virtual devices once initialized.
-        pass
-    ############ ENVIRONMENT ###################
-    n_envs = 4
-    env_list = [gym.make('BipedalWalker-v3') for i in range(n_envs)]
-    continuous_actions = True
-    action_dim = 4
-    T = 1600
-    env_kwargs = {}
-    ############ NNI HYPERPARAMETERS ###########
-    params = nni.get_next_parameter()
-    lr_max_policy = lr_min_policy = lr_max_value = lr_min_value = params['lr']
-    stdev_offset = params['stdev_offset']
-    gamma = params['gamma']
-    kappa = params['kappa']
-    global_clipnorm = params['global_clipnorm']
-    ppo_clip = params['ppo_clip']
-    means_activation = params['means_activation']
-    units = params['units']
-    policy_num_hidden = value_num_hidden = [units, units/2, units/4]
-    ############ HYPERPARAMETERS ###############
-    policy_activation = 'relu'
-    action_clip = 'clip'
-    stdev_type = 'constant'
-    stdev_min = 1e-6
-    value_num_hidden = [500, 250, 100]
-    value_activation = 'relu'
-    value_normalization = False
-    value_type = 'time-aware'
-    optimizer = tf.keras.optimizers.Adam
-    nepochs = 10  # number of epochs to train each iteration
-    nsteps = 1000  # each iteration samples nsteps transitions from each environment
-    batch_size = 32  # mini-batch size for gradient updates
-    ############ AMOUNT OF TRAINING #############
-    total_transitions = 4000000  # total number of sampled transitions, combined over all environments
-    reward_threshold = 300  # stop training if last env.mem episodes are above this threshold
-    
+try:
+    # Disable all GPUS
+    tf.config.set_visible_devices([], 'GPU')
+    visible_devices = tf.config.get_visible_devices()
+    for device in visible_devices:
+        assert device.device_type != 'GPU'
+except:
+    # Invalid device or cannot modify virtual devices once initialized.
+    pass
 
-    # setup
-    policy_lr = LinearDecreaseLR(lr_max_policy, lr_min_policy, total_transitions, n_envs, nepochs, nsteps, batch_size)
-    value_lr = LinearDecreaseLR(lr_max_value, lr_min_value, total_transitions, n_envs, nepochs, nsteps, batch_size)
-    ppo, cur_states = train_setup(env_list, continuous_actions, action_dim, T, env_kwargs, policy_num_hidden,
-                policy_activation, action_clip, means_activation, stdev_type, stdev_offset, stdev_min,
-                value_num_hidden, value_activation, value_normalization, value_type,
-                gamma, kappa, ppo_clip, global_clipnorm, optimizer, policy_lr, value_lr)
-    # training loop and reporting
-    n_updates = total_transitions // (n_envs*nsteps)
-    pbar = range(n_updates)
-    for i in pbar:
-        cur_states = ppo.step(cur_states, nepochs, nsteps, batch_size)
-        ep_rewards, ep_lens, ev, new_rewards = ppo.env.return_statistics()
-        nni.report_intermediate_result(np.mean(ep_rewards))
-        if np.mean(ep_rewards) > reward_threshold:
-            break
+############ ENVIRONMENT ###################
+n_envs = 4
+env_list = [gym.make('BipedalWalker-v3') for i in range(n_envs)]
+continuous_actions = True
+action_dim = 4
+T = 1600
+env_kwargs = {}
+############ NNI HYPERPARAMETERS ###########
+params = nni.get_next_parameter()
+lr_max_policy = lr_min_policy = lr_max_value = lr_min_value = params['lr']
+stdev_offset = params['stdev_offset']
+gamma = params['gamma']
+kappa = params['kappa']
+global_clipnorm = params['global_clipnorm']
+global_clipnorm = None if global_clipnorm==0 else global_clipnorm
+ppo_clip = params['ppo_clip']
+means_activation = params['means_activation']
+units = params['units']
+policy_num_hidden = value_num_hidden = [units, units/2, units/4]
+############ HYPERPARAMETERS ###############
+policy_activation = 'relu'
+action_clip = 'clip'
+stdev_type = 'constant'
+stdev_min = 1e-6
+value_activation = 'relu'
+value_normalization = False
+value_type = 'time-aware'
+optimizer = tf.keras.optimizers.Adam
+nepochs = 10  # number of epochs to train each iteration
+nsteps = 1000  # each iteration samples nsteps transitions from each environment
+batch_size = 32  # mini-batch size for gradient updates
+############ AMOUNT OF TRAINING #############
+total_transitions = 4000000  # total number of sampled transitions, combined over all environments
+reward_threshold = 300  # stop training if last env.mem episodes are above this threshold
 
-    nni.report_final_result(np.mean(ep_rewards))
+
+# setup
+policy_lr = LinearDecreaseLR(lr_max_policy, lr_min_policy, total_transitions, n_envs, nepochs, nsteps, batch_size)
+value_lr = LinearDecreaseLR(lr_max_value, lr_min_value, total_transitions, n_envs, nepochs, nsteps, batch_size)
+ppo, cur_states = train_setup(env_list, continuous_actions, action_dim, T, env_kwargs, policy_num_hidden,
+            policy_activation, action_clip, means_activation, stdev_type, stdev_offset, stdev_min,
+            value_num_hidden, value_activation, value_normalization, value_type,
+            gamma, kappa, ppo_clip, global_clipnorm, optimizer, policy_lr, value_lr)
+# training loop and reporting
+n_updates = total_transitions // (n_envs*nsteps)
+for i in range(n_updates):
+    cur_states = ppo.step(cur_states, nepochs, nsteps, batch_size)
+    ep_rewards, ep_lens, ev, new_rewards = ppo.env.return_statistics()
+    nni.report_intermediate_result(np.mean(ep_rewards))
+    if np.mean(ep_rewards) > reward_threshold:
+        break
+
+nni.report_final_result(np.mean(ep_rewards))
