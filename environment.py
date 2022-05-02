try to fix value normalization

environment.py
@@ -110,7 +110,7 @@ class TFEnv:
         return self.rewards[self.rewards!=0.], self.ep_lens[self.ep_lens!=0.], self.EVs, out
 
     def reset(self):
-        """Resets all environments and returns initial states."""
+        """For initialization purposes, resets all environments and returns initial states."""
         init_states = [env.reset() for env in self.env_list]
         init_states = np.stack(init_states, axis=0)
         return tf.convert_to_tensor(init_states, dtype=tf.float32)

models.py
@@ -209,7 +209,6 @@ class TimeAwareValue(SimpleMLP):
     """Regular MLP which learns a time-aware value function (https://arxiv.org/abs/1802.10031)."""
     def __init__(self, num_hidden, activation='tanh'):
         super().__init__(num_hidden, activation, 2)
-        self.n, self.mean, self.M = None, None, None
 
     def get_values(self, states, times, gamma, T):
         """Given a batch of states, returns the value function estimates.
@@ -229,39 +228,27 @@ class TimeAwareValue(SimpleMLP):
         return values
 
     def normalize_returns(self, returns, *args):
-        return returns, None, None, None
+        return returns
 
 class TimeAwareValue2(SimpleMLP):
-    """Time-aware value function with seperate networks for offset and reward rate."""
+    """Regular MLP which takes in time as well as the state."""
     def __init__(self, num_hidden, activation='tanh'):
         super().__init__(num_hidden, activation, 1)
-        self.offset_layers = [tf.keras.layers.Dense(i, activation=activation) for i in num_hidden]
-        self.offset_out = tf.keras.layers.Dense(1)
-        self.n, self.mean, self.M = None, None, None
 
-    def call(self, states):
-        values = super().call(states)[:,0]
-        offsets = states
-        for layer in self.offset_layers:
-            offsets = layer(offsets)
-        offsets = self.offset_out(offsets)[:,0]
-        return values, offsets
-
-    def get_values(self, states, times, gamma, T):
-        values, offsets = self.call(states)
-        return values*(1 - tf.math.pow(gamma, T-times+1))/(1-gamma)+offsets
+    def get_values(self, states, times, *args):
+        values = self.call(tf.concat([states, times], axis=1))
+        return values[:,0]
 
     def unnormalize_values(self, values):
         return values
 
     def normalize_returns(self, returns, *args):
-        return returns, None, None, None
+        return returns
 
 class RegularValue(SimpleMLP):
     """Regular MLP which learns a regular value function."""
     def __init__(self, num_hidden, activation='tanh'):
         super().__init__(num_hidden, activation, 1)
-        self.n, self.mean, self.M = None, None, None
 
     def get_values(self, states, *args):
         return self.call(states)[:,0]
@@ -270,36 +257,32 @@ class RegularValue(SimpleMLP):
         return values
 
     def normalize_returns(self, returns, *args):
-        return returns, None, None, None
+        return returns
 
-
-def update_running_mean_std(data, n, mean, M):
-    for i in data:
-        n = n+1
-        delta = i-mean
-        mean = mean+delta/n
-        delta2 = i-mean
-        M = M+delta*delta2
-    return n, mean, M
 
 def normalize_value(value):
     """Decorates value function approximator by adding normalization."""
     class NormalizedValue(value):
         def __init__(self, *args, **kwargs):
             super().__init__(*args, **kwargs)
-            self.n = tf.cast(0., tf.float32)
-            self.mean = tf.cast(0., tf.float32)
-            self.M = tf.cast(0., tf.float32)
+            self.n = tf.Variable(tf.cast(0., tf.float32), False, True)
+            self.mean = tf.Variable(tf.cast(0., tf.float32), False, True)
+            self.M = tf.Variable(tf.cast(0., tf.float32), False, True)
 
         def unnormalize_values(self, values):
-            if self.n==0:
+            if self.n==0.:
                 return values
             else:
                 return values*(self.M/self.n)**.5+self.mean
 
         def normalize_returns(self, returns, n, mean, M):
             """Update empirical mean/std and calculate normalized returns."""
-            n, mean, M = update_running_mean_std(tf.reshape(returns, [-1]), n, mean, M)
-            return (returns - mean)/(M/n)**.5, n, mean, M
+            for i in tf.reshape(returns, [-1]):
+                self.n.assign_add(1.)
+                delta = i-self.mean
+                self.mean.assign_add(delta/self.n)
+                delta2 = i-self.mean
+                self.M.assign_add(delta*delta2)
+            return (returns - self.mean)/(self.M/self.n)**.5
 
     return NormalizedValue

train.py
@@ -10,37 +10,38 @@ if __name__ == '__main__':
     # documentation in train_setup
     ############ ENVIRONMENT ###################
     n_envs = 4
-    env_list = [gym.make('BipedalWalker-v3') for i in range(n_envs)]
+    env_list = [gym.make('BipedalWalkerHardcore-v3') for i in range(n_envs)]
     continuous_actions = True
     action_dim = 4
-    T = 1600
+    T = 2000
     env_kwargs = {}
     ############ HYPERPARAMETERS ###############
-    policy_num_hidden = [500, 250, 100]
+    policy_num_hidden = [1000, 500, 200]
     policy_activation = 'relu'
     action_clip = 'clip'
     means_activation = 1.1
     stdev_type = 'constant'
-    stdev_offset = 0.69
-    stdev_min = 1e-3
-    value_num_hidden = [500, 250, 100]
+    stdev_offset = 0.5
+    stdev_min = 1e-6
+    value_num_hidden = [1000, 500, 200]
     value_activation = 'relu'
     value_normalization = False
     value_type = 'time-aware'
     gamma = 0.99
-    kappa = 0.9
+    kappa = 0.98
     ppo_clip = 0.2
     global_clipnorm = None
     optimizer = tf.keras.optimizers.Adam
-    lr_max_policy = 4e-4  # policy learning rate on first iteration
-    lr_min_policy = 5e-5  # policy learning rate on last iteration
-    lr_max_value = 4e-4
-    lr_min_value = 5e-5
+    lr_max_policy = 1e-4  # policy learning rate on first iteration
+    lr_min_policy = 1e-4  # policy learning rate on last iteration
+    lr_max_value = 1e-4
+    lr_min_value = 1e-4
     nepochs = 10  # number of epochs to train each iteration
     nsteps = 1000  # each iteration samples nsteps transitions from each environment
     batch_size = 32  # mini-batch size for gradient updates
     ############ AMOUNT OF TRAINING #############
-    total_transitions = 8000000  # total number of sampled transitions, combined over all environments
+    total_transitions = 4000000  # total number of sampled transitions, combined over all environments
+    reward_threshold = 300  # stop training if last env.mem episodes are above this threshold
 
     # setup
     policy_lr = LinearDecreaseLR(lr_max_policy, lr_min_policy, total_transitions, n_envs, nepochs, nsteps, batch_size)
@@ -61,6 +62,8 @@ if __name__ == '__main__':
         pbar.set_description('Iteration {:.0f}'.format(i+1))
         pbar.set_postfix_str('Avg ep reward={:.0f}, Avg ep len={:.0f}, Explained var={:.2f}'.format(
             np.mean(ep_rewards), np.mean(ep_lens), np.mean(ev))+', New ep rewards: '+new_rewards)
+        if np.mean(ep_rewards) > reward_threshold:
+            break
 
     plot_ep_rewards(ep_rewards_list, n_envs, nsteps)
 

train_setup.py
@@ -4,6 +4,7 @@ from models import DiscreteActor, MeanStdNetwork, MeanNetworkAndStdNetwork, Mean
 from ppo import PPO
 import matplotlib.pyplot as plt
 import numpy as np
+import tensorflow as tf
 
 
 def train_setup(env_list, continuous_actions, action_dim, T, env_kwargs, policy_num_hidden,
@@ -56,7 +57,7 @@ def train_setup(env_list, continuous_actions, action_dim, T, env_kwargs, policy_
     action_type = 'discrete' if not continuous_actions else action_clip
     tf_env = TFEnv(env_list, action_type, **env_kwargs)
     tf_env_step = make_tf_env_step(tf_env)
-    cur_states = tf_env.reset()
+    cur_states, cur_times = tf_env.reset(), tf.convert_to_tensor(tf_env.num_steps, tf.float32)
     # make policy -  also make the output layer kernel weights smaller, for a more uniform initial policy
     assert type(policy_num_hidden)==list
     if continuous_actions:
@@ -67,7 +68,7 @@ def train_setup(env_list, continuous_actions, action_dim, T, env_kwargs, policy_
         policy = add_tanh_clipping(policy, means_activation) if action_clip=='tanh' \
             else add_clipping(policy, means_activation)
         policy = policy(policy_num_hidden, policy_activation, action_dim, stdev_offset, stdev_min)
-        policy(cur_states)
+        policy(cur_states)  # initialize weights
         if stdev_type=='combined':
             output_kernel, output_bias = policy.layers[-1].get_weights()
             policy.layers[-1].set_weights([output_kernel/100, output_bias])
@@ -81,7 +82,7 @@ def train_setup(env_list, continuous_actions, action_dim, T, env_kwargs, policy_
             policy.layers[-2].set_weights([output_kernel/100, output_bias])
     else:
         policy = DiscreteActor(action_dim, policy_num_hidden, policy_activation)
-        policy(cur_states)
+        policy(cur_states)  # initialize weights
         output_kernel, output_bias = policy.layers[-1].get_weights()
         policy.layers[-1].set_weights([output_kernel/100, output_bias])
     # make value function
@@ -89,8 +90,8 @@ def train_setup(env_list, continuous_actions, action_dim, T, env_kwargs, policy_
     value_options = {'regular':RegularValue, 'time-aware':TimeAwareValue, 'time-aware-2':TimeAwareValue2}
     value = value_options[value_type]
     value = value if not value_normalization else normalize_value(value)
-    value = value(value_num_hidden, value_activation)
-    value(cur_states)
+    value = value(value_num_hidden, value_activation)  
+    value.get_values(cur_states, cur_times, gamma, T)  # initialize weights
     # make optimizers
     policy_optimizer = optimizer(learning_rate=policy_lr, global_clipnorm=global_clipnorm)
     value_optimizer = optimizer(learning_rate=value_lr, global_clipnorm=global_clipnorm)
@@ -123,3 +124,4 @@ def plot_ep_rewards(ep_rewards_list, n_envs, nsteps):
     plt.fill_between(x, y-y_std, y+y_std, alpha=0.2, color='C1')
     plt.ylabel('reward')
     plt.xlabel('number of transitions')
+    plt.show()